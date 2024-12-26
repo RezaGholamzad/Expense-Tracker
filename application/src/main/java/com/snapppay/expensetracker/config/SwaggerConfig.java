bbcbc8d2324aaf473c9ea828b98b0c873f232c27
r.gholamzad <gholamzadreza@gmail.com>
2024-12-26 21:24:35 +0330

    add swagger to project

---
+++ application/src/main/java/com/snapppay/expensetracker/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.snapppay.expensetracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Expense-tracker")
+                .description("Expense-tracker APIs")
+                .version("1.0")
+        );
+    }
+
+}
