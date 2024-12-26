d448c0326dee0695d6b756bb3b5e38e88566488e
r.gholamzad <gholamzadreza@gmail.com>
2024-12-26 20:23:38 +0330

    project modularization based on layered architecture

--- application/src/main/java/com/snapppay/expensetracker/ExpenseTrackerApplication.java
+++ application/src/main/java/com/snapppay/expensetracker/ExpenseTrackerApplication.java
@@ -1,9 +1,11 @@
 package com.snapppay.expensetracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.snapppay.expensetracker")
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
