a43773325fc64932cc302dd00aa1075b348d315d
r.gholamzad <gholamzadreza@gmail.com>
2024-12-29 12:10:34 +0330

    write custom toString to login request and response (don't print raw password and token in the log)

--- src/main/java/com/snapppay/expensetracker/model/LoginRequest.java
+++ src/main/java/com/snapppay/expensetracker/model/LoginRequest.java
@@ -3,5 +3,11 @@ package com.snapppay.expensetracker.model;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginRequest(@Schema(requiredMode = Schema.RequiredMode.REQUIRED) String username,
-                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String password){
+                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) String password) {
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ": username: " +
+                username + "\n" +
+                "password: " + "*********";
+    }
 }
--- src/main/java/com/snapppay/expensetracker/model/LoginResponse.java
+++ src/main/java/com/snapppay/expensetracker/model/LoginResponse.java
@@ -1,4 +1,12 @@
 package com.snapppay.expensetracker.model;
 
 public record LoginResponse(String token, String username, String firstName, String lastName) {
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + ": username: " +
+                username + "\n" + "firstName: " +
+                firstName + "\n" + "lastName: "
+                + lastName + "\n" +
+                "token: " + "*********";
+    }
 }
