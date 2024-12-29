69812b6cab6bb015d7e02422c9c962ea16d2d332
r.gholamzad <gholamzadreza@gmail.com>
2024-12-29 11:24:34 +0330

    code improving(remove unnecessary annotations)

--- src/main/java/com/snapppay/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/snapppay/expensetracker/ExpenseTrackerApplication.java
@@ -1,17 +1,11 @@
 package com.snapppay.expensetracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
-@ComponentScan("com.snapppay.expensetracker")
+@SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaRepositories(basePackages = "com.snapppay.expensetracker.repository")
-@EntityScan(basePackages = "com.snapppay.expensetracker.entity")
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
